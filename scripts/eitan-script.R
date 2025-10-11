c0e32fe6b34d57e72cff25121620333d33c5db2a
Eitan Boaz <eitan@boazim.com>
2025-10-11 14:58:07 -0700

    workiing with merged cleana

--- scripts/eitan-script.R
+++ scripts/eitan-script.R
@@ -5,10 +5,7 @@ interest <- read.csv("../data/interest-clean.csv")
 
 merged_data <- inner_join(background, interest, by = "Response.ID")
 
-removed_background <- nrow(background) - nrow(merged_data)
-removed_interest <- nrow(interest) - nrow(merged_data)
-
-print(summary(merged_data))
 
 write.csv(merged_data, "../results/merged_clean.csv", row.names = FALSE)
+
 
