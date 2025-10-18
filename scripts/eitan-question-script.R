9f84565296fe6e2a2cdedcf6cae80fd2b598a304
Eitan Boaz <eboaz@ucsb.edu>
2025-10-18 10:25:17 -0700

    Update eitan-question-script.R

--- scripts/eitan-question-script.R
+++ scripts/eitan-question-script.R
@@ -11,3 +11,28 @@ int_long <- df %>%
   mutate(area = canon(area)) %>%
   filter(area != "") %>%
   distinct(row_id, area)
+
+# top interests plot
+top_n <- 10
+top_interests <- int_long %>%
+  count(area, sort = TRUE) %>%
+  slice_head(n = top_n)
+
+ggplot(top_interests, aes(x = reorder(area, n), y = n)) +
+  geom_col() +
+  coord_flip() +
+  labs(title = "Top interests (overall)", x = NULL, y = "Count")
+
+
+
+course_df <- df %>%
+  mutate(row_id = row_number()) %>%
+  select(row_id, `PSTAT120`, CS16, PSTAT131, CS130) %>%
+  mutate(across(-row_id, ~{
+    v <- suppressWarnings(as.numeric(.x))
+    v[is.na(v)] <- 0
+    as.integer(pmin(1, pmax(0, round(v))))
+  }))
+
+# Only show these exact courses (which I though would be the most interesting)
+courses_to_show <- c("PSTAT120", "CS16", "PSTAT131", "CS130")
