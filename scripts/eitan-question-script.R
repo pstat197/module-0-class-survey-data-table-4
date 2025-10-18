f657c029d26b6d750aa7253bccf78f91da5cac0f
Eitan Boaz <eboaz@ucsb.edu>
2025-10-18 10:25:33 -0700

    added second grapb

--- scripts/eitan-question-script.R
+++ scripts/eitan-question-script.R
@@ -36,3 +36,40 @@ course_df <- df %>%
 
 # Only show these exact courses (which I though would be the most interesting)
 courses_to_show <- c("PSTAT120", "CS16", "PSTAT131", "CS130")
+
+# ---- small interest-wide: just the top interests ----
+int_wide_small <- tibble(row_id = seq_len(nrow(df))) %>%
+  left_join(
+    int_long %>%
+      filter(area %in% top_interests$area) %>%
+      mutate(val = 1L) %>%
+      pivot_wider(names_from = area, values_from = val, values_fill = 0L),
+    by = "row_id"
+  ) %>%
+  mutate(across(-row_id, ~ replace_na(.x, 0L)))
+
+# ---- build rates: % with interest for takers vs non-takers, per course ----
+rates <- map_dfr(courses_to_show, function(crs) {
+  course_df %>%
+    select(row_id, took = all_of(crs)) %>%
+    left_join(int_wide_small, by = "row_id") %>%
+    mutate(group = if_else(took == 1L, "took", "didn't")) %>%
+    select(-row_id, -took) %>%
+    pivot_longer(-group, names_to = "interest", values_to = "has_interest") %>%
+    group_by(group, interest) %>%
+    summarise(pct = mean(has_interest), .groups = "drop") %>%
+    mutate(course = crs)
+})
+
+# ---- plot ----
+ggplot(rates, aes(x = interest, y = pct, fill = group)) +
+  geom_col(position = "dodge") +
+  facet_wrap(~ course, scales = "free_x") +
+  scale_y_continuous(labels = scales::percent_format()) +
+  coord_flip() +
+  labs(
+    title = "Do courses line up with interests?",
+    subtitle = "Share selecting each interest, among students who took vs didn't take the course",
+    x = NULL, y = "% of students", fill = NULL
+  ) +
+  theme(legend.position = "top")
